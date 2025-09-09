85f69f2b36c110bc4cb4c8be770f12e145a0fb04
Ekin <1197026828@qq.com>
2025-09-09 20:16:06 +1000

    debug

--- backend/api/index.js
+++ backend/api/index.js
@@ -26,6 +26,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "public")));
 
 const catchErrors = (fn) => async (req, res) => {
   try {
